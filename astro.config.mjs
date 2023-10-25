306122fe6d4742a61ca273214d6c69893dee3d63
Chening Duker <cdduker@gmail.com>
2023-10-25 01:53:23 -0400

    Get SolidJS interactive

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import solid from "@astrojs/solid-js";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    solid({
+      include: ["**/solid/*"],
+    }),
+  ],
+});
---
+++ src/components/solid/TestButton.tsx
@@ -0,0 +1,18 @@
+import { createSignal } from "solid-js";
+
+const [count, setCount] = createSignal(0);
+export default function TestButton({ children }: any) {
+  const add = () => setCount(count() + 1);
+  const subtract = () => setCount(count() - 1);
+
+  return (
+    <>
+      <div class="counter">
+        <button onClick={subtract}>-</button>
+        <pre>{count()}</pre>
+        <button onClick={add}>+</button>
+      </div>
+      <div class="counter-message">{children}</div>
+    </>
+  );
+}
