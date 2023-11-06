ec03204e82c6dea2579b578dbbf4c883c462f38b
Chening Duker <cdduker@gmail.com>
2023-11-05 19:23:53 -0500

    deploy to cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
