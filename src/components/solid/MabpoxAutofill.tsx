f03e713849fff73499cc84f2d5a3822928ce90aa
Jimy Marletta <84329916+honocoder@users.noreply.github.com>
2023-11-10 17:10:37 +0100

    implement solid component for mapbox search

--- src/components/solid/MabpoxAutofill.tsx
+++ src/components/solid/MabpoxAutofill.tsx
@@ -1,19 +1,19 @@
-import { onMount } from 'solid-js';
+import { onMount } from "solid-js";
 
 // TODO: this component sounds OK but it doesn't work
 const MapboxAutofill = () => {
   onMount(() => {
-    const script = document.createElement('script');
-    script.src = 'https://api.mapbox.com/search-js/v1.0.0-beta.17/web.js';
+    const script = document.createElement("script");
+    script.src = "https://api.mapbox.com/search-js/v1.0.0-beta.17/web.js";
     script.defer = true;
     document.body.appendChild(script);
 
     script.onload = () => {
-      console.log('mapbox loaded: ', mapboxsearch);
+      console.log("mapbox loaded: ", mapboxsearch);
       // @ts-ignore
       mapboxsearch.autofill({
         accessToken:
-          'pk.eyJ1IjoidG9vbHMtZ3AtdXNlciIsImEiOiJjbG9wdzcyZHYwZDZyMmpwcWdhZ3Y5eGFwIn0.gjla1E8tbBJNA5_ACCWNPQ',
+          "pk.eyJ1IjoidG9vbHMtZ3AtdXNlciIsImEiOiJjbG9wdzcyZHYwZDZyMmpwcWdhZ3Y5eGFwIn0.gjla1E8tbBJNA5_ACCWNPQ",
       });
     };
   });
