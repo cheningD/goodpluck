5501dd2562d6b149bceeb9fdc5f835ee013da443
wpplumber <mail.tarik.rital@gmail.com>
2023-11-13 09:39:18 +0100

    opt(project): Add unplugin-icons with auto install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,36 @@
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), tailwind()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    resolve: {
+     alias: {
+      "@": "./src",
+     }
+    },
+    plugins: [
+    /* ... */
+    AutoImport({
+      resolvers: [
+        IconsResolver({
+          prefix: 'Icon',
+          extension: 'jsx',
+        }),
+      ],
+    }),
+    Icons({
+      compiler: 'solid', // or 'solid',
+      autoInstall: true,
+    }),
+    ]
+  }
 });
