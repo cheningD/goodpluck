78c3529ab05af3d8dde77ba3667eae4f896c6ba1
Chening Duker <cdduker@gmail.com>
2023-11-13 21:35:18 -0500

    fail tests fast

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,11 +16,13 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+  /* Fail the build on CI if there are too many failures or flakes. */
+  maxFailures: process.env.CI ? 1 : undefined,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   webServer: {
     command: "npm run start",
