6fa2aafa4205f3d98b73fb66a41790f8aeb424d3
Chening Duker <cdduker@gmail.com>
2023-11-14 03:21:11 -0500

    remove sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,11 @@
 import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
-import sentry from "@sentry/astro";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
-const SENTRY_AUTH_TOKEN = "import.meta.env.SENTRY_AUTH_TOKEN";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    solid(),
-    tailwind(),
-    sentry({
-      dsn: "https://2e3c5757571691fb195c2e89dfd7bf57@o469653.ingest.sentry.io/4506223026634752",
-      sourceMapsUploadOptions: {
-        project: "javascript-astro",
-        authToken: SENTRY_AUTH_TOKEN,
-        org: "goodpluck",
-      },
-    }),
-  ],
+  integrations: [solid(), tailwind()],
   output: "server",
   adapter: cloudflare(),
-  vite: {
-    define: {
-      "import.meta.env.SENTRY_AUTH_TOKEN": import.meta.env.SENTRY_AUTH_TOKEN,
-    },
-  },
 });
