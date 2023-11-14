2898fea73c933f2cc2a908074e1c0e5a05b298b5
Chening Duker <cdduker@gmail.com>
2023-11-14 02:29:54 -0500

    add sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
+import sentry from "@sentry/astro";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), tailwind()],
+  integrations: [
+    solid(),
+    tailwind(),
+    sentry({
+      dsn: "https://2e3c5757571691fb195c2e89dfd7bf57@o469653.ingest.sentry.io/4506223026634752",
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
   output: "server",
   adapter: cloudflare(),
 });
