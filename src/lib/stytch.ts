fab3f434aa43d4d6b2fb7c0bee9a94fd947bc9cf
wpplumber <mail.tarik.rital@gmail.com>
2023-11-18 17:19:50 +0100

    opt(project): uses Vite’s built-in support for environment variables

--- src/lib/stytch.ts
+++ src/lib/stytch.ts
@@ -1,16 +1,16 @@
 import * as stytch from "stytch";
 
-if (!process.env.STYTCH_PROJECT_ID) {
+if (!import.meta.env.STYTCH_PROJECT_ID) {
   throw new Error("Missing STYTCH_PROJECT_ID env var");
 }
 
-if (!process.env.STYTCH_PROJECT_SECRET) {
+if (!import.meta.env.STYTCH_PROJECT_SECRET) {
   throw new Error("Missing STYTCH_PROJECT_SECRET env var");
 }
 
 const client = new stytch.Client({
-  project_id: process.env.STYTCH_PROJECT_ID,
-  secret: process.env.STYTCH_PROJECT_SECRET,
+  project_id: import.meta.env.STYTCH_PROJECT_ID,
+  secret: import.meta.env.STYTCH_PROJECT_SECRET,
 });
 
 // Stytch is currently broken in cloudflare runtime so we use this patch
