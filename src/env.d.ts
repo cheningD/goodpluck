f58c0927be7a29f7c522db3ef71828e26565f41c
“ashra-f” <appletreemann@protonmail.com>
2023-11-20 18:06:49 -0500

    Add environment variables to Vite config

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,16 +1,1 @@
 /// <reference types="astro/client" />
-
-type ENV = {
-  STYTCH_PROJECT_ID: string;
-  STYTCH_PROJECT_SECRET: string;
-  SWELL_STORE_ID: string;
-  SWELL_SECRET_KEY: string;
-};
-
-// Depending on your adapter mode
-// use `AdvancedRuntime<ENV>` for advance runtime mode
-// use `DirectoryRuntime<ENV>` for directory runtime mode
-type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<ENV>;
-declare namespace App {
-  interface Locals extends Runtime { }
-}
