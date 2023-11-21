d922547fcc955bd22c9785d9a7f6263061816f48
Chening Duker <cdduker@gmail.com>
2023-11-21 15:25:50 -0500

    cleanup and restore login page

--- src/lib/swell.ts
+++ src/lib/swell.ts
@@ -1,5 +1,7 @@
 import swell from "swell-js";
 
+// Initialize Swell, Access the envirnoment variables from the runtime
+// https://docs.astro.build/en/guides/integrations-guide/cloudflare/#cloudflare-runtime
 function initSwell(storeId: string, publicKey: string) {
   swell.init(storeId, publicKey);
   return swell;
