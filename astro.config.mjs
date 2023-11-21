7645e9a275613afeb92994572094952bb40a638c
“ashra-f” <appletreemann@protonmail.com>
2023-11-21 00:26:08 -0500

    Update Astro config to use local runtime mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [solid(), tailwind()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    runtime: { mode: "local" },
+  }),
 });
