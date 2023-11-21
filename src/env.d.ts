6ebd44ceb5615a6cff2029af7174695e4bae1039
Chening Duker <cdduker@gmail.com>
2023-11-21 15:25:50 -0500

    cleanup and restore login page

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,16 +1,5 @@
-/// <reference types="astro/client" />
+/// <reference types="astro/client" />\
 
-type ENV = {
-  // Add your environment variables here
-  STYTCH_PROJECT_ID: string;
-  STYTCH_PROJECT_SECRET: string;
-  SWELL_STORE_ID: string;
-  SWELL_SECRET_KEY: string;
-};
-
-// Depending on your adapter mode
-// use `AdvancedRuntime<ENV>` for advance runtime mode
-// use `DirectoryRuntime<ENV>` for directory runtime mode
 type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<ENV>;
 declare namespace App {
   interface Locals extends Runtime { }
