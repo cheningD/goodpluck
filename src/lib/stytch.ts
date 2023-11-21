69d9a758dadc8f6fca93f0564a0f4786764ce3d7
“ashra-f” <appletreemann@protonmail.com>
2023-11-21 13:41:12 -0500

    Refactor Stytch client initialization and usage

--- src/lib/stytch.ts
+++ src/lib/stytch.ts
@@ -1,34 +1,38 @@
 import * as stytch from "stytch";
 
-export const createStytchClient = (env: any) => {
-  const { STYTCH_PROJECT_ID, STYTCH_PROJECT_SECRET } = env;
+let stytchClientInstance: stytch.Client | null = null; // Singleton
 
-  if (!STYTCH_PROJECT_ID) {
-    throw new Error("Missing STYTCH_PROJECT_ID env var");
+export const getStytchClient = (runtime: any) => {
+  if (!stytchClientInstance) {
+    const { STYTCH_PROJECT_ID, STYTCH_PROJECT_SECRET } = runtime.env;
+
+    if (!STYTCH_PROJECT_ID) {
+      throw new Error("Missing STYTCH_PROJECT_ID env var");
+    }
+
+    if (!STYTCH_PROJECT_SECRET) {
+      throw new Error("Missing STYTCH_PROJECT_SECRET env var");
+    }
+
+    stytchClientInstance = new stytch.Client({
+      project_id: STYTCH_PROJECT_ID,
+      secret: STYTCH_PROJECT_SECRET,
+    });
+
+    // Stytch is currently broken in Cloudflare runtime so we use this patch
+    /* eslint-disable */
+    const cl = <any>stytchClientInstance;
+    /* eslint-enable */
+    cl.fetchConfig.cache = undefined;
   }
 
-  if (!STYTCH_PROJECT_SECRET) {
-    throw new Error("Missing STYTCH_PROJECT_SECRET env var");
-  }
-
-  const client = new stytch.Client({
-    project_id: STYTCH_PROJECT_ID,
-    secret: STYTCH_PROJECT_SECRET,
-  });
-
-  // Stytch is currently broken in cloudflare runtime so we use this patch
-  /* eslint-disable */
-  const cl = <any>client;
-  /* eslint-enable */
-  cl.fetchConfig.cache = undefined;
-
-  return client;
+  return stytchClientInstance;
 };
 
-export const isLoggedIn = async (session_token: string, env: any) => {
+export const isLoggedIn = async (session_token: string, runtime: any) => {
   if (session_token) {
     try {
-      const stytchclient = createStytchClient(env);
+      const stytchclient = getStytchClient(runtime);
       // Verify the session token with Stytch
       await stytchclient.sessions.authenticate({ session_token });
       return true; // Session is valid
