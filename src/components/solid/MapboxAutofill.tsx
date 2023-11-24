8ade0c732dbf179f23295f4129311776bda81c6c
“ashra-f” <appletreemann@protonmail.com>
2023-11-24 14:17:35 -0500

    Hide previously exposed API token

--- src/components/solid/MapboxAutofill.tsx
+++ src/components/solid/MapboxAutofill.tsx
@@ -1,7 +1,11 @@
 import { onMount } from "solid-js";
 
+interface MapboxAutofillProps {
+  readonly mapboxToken: string;
+}
+
 // TODO: this component sounds OK but it doesn't work
-const MapboxAutofill = () => {
+const MapboxAutofill = ({ mapboxToken }: MapboxAutofillProps) => {
   onMount(() => {
     const script = document.createElement("script");
     script.src = "https://api.mapbox.com/search-js/v1.0.0-beta.17/web.js";
@@ -9,11 +13,10 @@ const MapboxAutofill = () => {
     document.body.appendChild(script);
 
     script.onload = () => {
-      console.log("mapbox loaded: ", mapboxsearch);
+      // console.log("mapbox loaded: ", mapboxsearch);
       // @ts-ignore
       mapboxsearch.autofill({
-        accessToken:
-          "pk.eyJ1IjoidG9vbHMtZ3AtdXNlciIsImEiOiJjbG9wdzcyZHYwZDZyMmpwcWdhZ3Y5eGFwIn0.gjla1E8tbBJNA5_ACCWNPQ",
+        accessToken: mapboxToken,
       });
     };
   });
