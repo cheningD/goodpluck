f91e45a0f91e1c017b909f3d1ea25d2ce3f52a7a
“ashra-f” <appletreemann@protonmail.com>
2023-11-26 15:26:41 -0500

    Check session cookie deletion

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -176,18 +176,28 @@ test.describe("Validate Login Code", () => {
 test.describe("Logout", () => {
   test("should successfully log out a user if a session exists", async ({
     page,
+    browserName,
   }) => {
-    // // Check if the user is redirected to the homepage with a toast message
-    // await page.goto(`/logout`);
-    // const message = "Successfully logged out"
-    // const encodedMessage = encodeURIComponent(message);
-    // expect(page.url()).toContain(`/?message=${encodedMessage}`);
-    // // Check if the session cookie is deleted
-    // const cookies = await page.context().cookies();
-    // const sessionCookie = cookies.find(
-    //   (cookie) => cookie.name === "gp_session_token",
-    // );
-    // expect(sessionCookie).toBeUndefined();
+    test.skip(
+      browserName === "webkit" && isDevelopment,
+      "Safari won't let you set a cookie on localhost without https in development environment",
+    );
+
+    // OTP Login with Valid Email
+    await page.goto(`/login`);
+    await page.waitForTimeout(2000);
+    await page.getByLabel("Email").fill("sandbox@stytch.com");
+    await page.getByTestId("login-btn").click();
+    await page.fill("#otp-input", "000000");
+    await page.click('button[id="submit-login-code-btn"]');
+
+    // Check if the session cookie is deleted
+    await page.goto(`/logout`);
+    const cookies = await page.context().cookies();
+    const sessionCookie = cookies.find(
+      (cookie) => cookie.name === "gp_session_token",
+    );
+    expect(sessionCookie).toBeUndefined();
   });
 
   test("should show an error for users who are not logged in and attempt to log out", async ({
