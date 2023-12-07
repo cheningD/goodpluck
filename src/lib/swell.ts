f09c337a338d1ba90c9547875f3864e08097dd90
wpplumber <mail.tarik.rital@gmail.com>
2023-12-07 18:17:45 +0100

    Apply reviews on the PR #32

--- src/lib/swell.ts
+++ src/lib/swell.ts
@@ -2,7 +2,9 @@ import swell from "swell-js";
 
 // Initialize Swell, Access the envirnoment variables from the runtime
 // https://docs.astro.build/en/guides/integrations-guide/cloudflare/#cloudflare-runtime
-export default function initSwell(storeId: string, publicKey: string) {
+function initSwell(storeId: string, publicKey: string) {
   swell.init(storeId, publicKey);
   return swell;
 }
+
+export { initSwell };
