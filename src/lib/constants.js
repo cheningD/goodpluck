11342f2f9a925266fb88c6807d73d4d5ab79b0a0
“ashra-f” <appletreemann@protonmail.com>
2023-12-10 00:43:21 -0500

    Redirect to waitlist

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,2 +1,3 @@
 export const session_duration_minutes = 60 * 24 * 30; // 30 days
 export const max_length_cloudflare_base_url = 56;
+export const confirmed_zipcodes = ["48201", "48202", "48203", "48204", "48205"];
