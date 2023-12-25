fe371e942fe9dba07ca91e00018421f13ce73871
wpplumber <mail.tarik.rital@gmail.com>
2023-12-25 17:39:44 +0100

    (WIP) Implement Custom Basket Functionality

--- src/components/solid/Cart.tsx
+++ src/components/solid/Cart.tsx
@@ -1,5 +1,7 @@
 import type { Cart as CartType } from "swell-js";
-import { onMount, createSignal, type Component } from 'solid-js'
+import { onMount, createSignal, createEffect, type Component, Show } from 'solid-js'
+import { formatDate } from '@composables/timeUtils'
+import { getDeliverySlots, type Basket } from '@composables/basketUtils'
 
 interface CartProps {
   readonly zipCodes: string[]
@@ -8,6 +10,20 @@ interface CartProps {
 const Cart: Component<CartProps> = ({ zipCodes }) => {
   const [zipCode, setZipCode] = createSignal<string>('')
   const [isValidZip, setIsValidZip] = createSignal<boolean | null>(null)
+  const [deliverySlots, setDeliverySlots] = createSignal<string[]>([])
+  const [selectedSlot, setSelectedSlot] = createSignal<string>('')
+  const [baskets, setBaskets] = createSignal<Basket[]>([])
+  const [activeBasket, setActiveBasket] = createSignal<Basket | null>(null)
+  // const [activeBasketIndex, setActiveBasketIndex] = createSignal<number>(0);
+
+  const addNewBasket = (): void => {
+    setActiveBasket({ items: [], deliveryDate: selectedSlot() })
+    setBaskets([...baskets(), { items: [], deliveryDate: selectedSlot() }])
+  }
+
+  createEffect(() => {
+    setDeliverySlots(getDeliverySlots())
+  })
 
   const validateZipCode = (): void => {
     const isValid: boolean = zipCodes.includes(zipCode())
@@ -42,7 +58,7 @@ const Cart: Component<CartProps> = ({ zipCodes }) => {
         data-testid="basket-sidebar"
         id="sidebar-mini"
         data-hs-overlay-keyboard="true"
-        class="relative md:absolute md:w-1/2 lg:w-1/3 min-h-[50vh] [--overlay-backdrop:null] hs-overlay hs-overlay-open:translate-x-0 translate-x-full  top-0 end-0 transition-all duration-300 transform h-full lg:max-w-sm z-[60] bg-white border-s dark:bg-gray-800 dark:border-gray-700 hidden"
+        class="relative md:absolute md:w-1/2 lg:w-1/3 min-h-[calc(100vh_-_108px)] [--overlay-backdrop:null] hs-overlay hs-overlay-open:translate-x-0 translate-x-full  top-0 end-0 transition-all duration-300 transform h-full lg:max-w-sm z-[60] bg-white border-s dark:bg-gray-800 dark:border-gray-700 hidden"
         tabindex="-1"
       >
         <nav
@@ -62,7 +78,9 @@ const Cart: Component<CartProps> = ({ zipCodes }) => {
                 aria-controls="bar-with-underline-3"
                 role="tab"
               >
-                Orders
+                <Show when={selectedSlot() === ''} fallback={selectedSlot()}>
+                  Select Date
+                </Show>
               </button>
             }
           >
@@ -87,80 +105,144 @@ const Cart: Component<CartProps> = ({ zipCodes }) => {
             aria-controls="bar-with-underline-2"
             role="tab"
           >
-            Select Date
+            Orders
           </button>
         </nav>
 
-        <div class="mt-3 h-full">
-          <Show
-            when={!isValidZip()}
-            fallback={
-              <div
-                id="bar-with-underline-3"
-                class=""
-                role="tabpanel"
-                aria-labelledby="bar-with-underline-item-3"
-              >
-                <h3 class="text-3xl text-yellow-800">Products list ...</h3>
-              </div>
-            }
-          >
+        <Show
+          when={!isValidZip()}
+          fallback={
             <div
-              id="bar-with-underline-1"
+              id="bar-with-underline-3"
+              class="h-full flex flex-col bg-white"
               role="tabpanel"
-              aria-labelledby="bar-with-underline-item-1"
-              class=" h-full"
+              aria-labelledby="bar-with-underline-item-3"
             >
-              <form
-                class="p-4 flex flex-col h-full gap-y-10"
-                onSubmit={handleSubmit}
-              >
-                <p class="text-gray-500 dark:text-gray-400">
-                  Before we add items to your order, let's{' '}
-                  <em class="font-semibold text-gray-800 dark:text-gray-200">
-                    confirm
-                  </em>{' '}
-                  we deliver to your area.
-                </p>
-                <div class="flex flex-col gap-y-3">
-                  <input
-                    type="number"
-                    class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-                    placeholder="Zip Code"
-                    id="zip"
-                    name="zip"
-                    value={zipCode()}
-                    onInput={(e) => setZipCode(e.target.value)}
-                    data-testid="user-zip"
-                  />
-                  <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
-                    Already have an account?
-                    <a
-                      class="text-blue-600 ml-1 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                      href="/login"
+              <Show
+                when={activeBasket() === null}
+                fallback={
+                  <>
+                    <h4 class="text-3xl">Add items to your basket.</h4>
+                    <button
+                      onClick={() => { addNewBasket() }}
+                      data-testid="btn-create-order"
+                      type="submit"
+                      class="w-3/4 uppercase mt-4 mx-auto py-3 px-4 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-orange-800 text-white hover:bg-orange-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                     >
-                      Sign In here
-                    </a>
-                  </p>
-                </div>
-                <button
-                  data-testid="btn-verify-zip"
-                  type="submit"
-                  class="w-3/4 uppercase mx-auto py-3 px-4 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-orange-800 text-white hover:bg-orange-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                >
-                  Check
-                </button>
-              </form>
-            </div>
-          </Show>
+                      Start Shopping
+                    </button>
+                  </>
+                }
+              >
+                <ul class="max-h-[70vh] overflow-y-auto">
+                  {deliverySlots().map((slot: string) => (
+                    <>
+                      <li
+                        onClick={() =>
+                          setSelectedSlot(
+                            formatDate(slot, 'en-US', {
+                              weekday: 'short',
+                              month: 'short',
+                              day: 'numeric'
+                            })
+                          )
+                        }
+                        class="px-3 py-4 cursor-pointer border-gray-300 border-b flex items-center justify-between"
+                      >
+                        <div class="">
+                          <h4 class="text-2xl font-bold">{slot}</h4>
+                          <span>Delivery time: 10:00AM</span>
+                        </div>
+                        {selectedSlot() ===
+                          formatDate(slot, 'en-US', {
+                            weekday: 'short',
+                            month: 'short',
+                            day: 'numeric'
+                          }) && (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-8 h-8 bg-orange-800 text-white rounded-full p-1"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M9 20c0 1.1-.9 2-2 2s-2-.9-2-2s.9-2 2-2s2 .9 2 2m8-2c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2m-9.8-3.2v-.1l.9-1.7h7.4c.7 0 1.4-.4 1.7-1l3.9-7l-1.7-1l-3.9 7h-7L4.3 2H1v2h2l3.6 7.6L5.2 14c-.1.3-.2.6-.2 1c0 1.1.9 2 2 2h12v-2H7.4c-.1 0-.2-.1-.2-.2M18 2.8l-1.4-1.4l-4.8 4.8l-2.6-2.6L7.8 5l4 4z"
+                            />
+                          </svg>
+                        )}
+                      </li>
+                    </>
+                  ))}
+                </ul>
 
+                <button
+                  onClick={() => { addNewBasket() }}
+                  data-testid="btn-create-order"
+                  type="submit"
+                  class="w-3/4 uppercase mt-2 mx-auto py-3 px-4 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-orange-800 text-white hover:bg-orange-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                >
+                  Create order
+                </button>
+              </Show>
+            </div>
+          }
+        >
           <div
-            id="bar-with-underline-2"
-            class="hidden"
+            id="bar-with-underline-1"
             role="tabpanel"
-            aria-labelledby="bar-with-underline-item-2"
-          ></div>
-        </div>
+            aria-labelledby="bar-with-underline-item-1"
+            class=" h-full"
+          >
+            <form
+              class="p-4 flex flex-col h-full gap-y-10"
+              onSubmit={handleSubmit}
+            >
+              <p class="text-gray-500 dark:text-gray-400">
+                Before we add items to your order, let's{' '}
+                <em class="font-semibold text-gray-800 dark:text-gray-200">
+                  confirm
+                </em>{' '}
+                we deliver to your area.
+              </p>
+              <div class="flex flex-col gap-y-3">
+                <input
+                  type="number"
+                  class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                  placeholder="Zip Code"
+                  id="zip"
+                  name="zip"
+                  value={zipCode()}
+                  onInput={(e) => setZipCode(e.target.value)}
+                  data-testid="user-zip"
+                />
+                <p class="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                  Already have an account?
+                  <a
+                    class="text-blue-600 ml-1 decoration-2 hover:underline font-medium dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    href="/login"
+                  >
+                    Sign In here
+                  </a>
+                </p>
+              </div>
+              <button
+                data-testid="btn-verify-zip"
+                type="submit"
+                class="w-3/4 uppercase mx-auto py-3 px-4 inline-flex items-center justify-center gap-x-2 text-sm font-semibold rounded-full border border-transparent bg-orange-800 text-white hover:bg-orange-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              >
+                Check
+              </button>
+            </form>
+          </div>
+        </Show>
+
+        {/* <div
+          id="bar-with-underline-2"
+          class="hidden"
+          role="tabpanel"
+          aria-labelledby="bar-with-underline-item-2"
+        ></div> */}
+        {/* </div> */}
       </div>
     </>
   )
---
+++ src/composables/basketUtils.ts
@@ -0,0 +1,33 @@
+import { formatDate } from './timeUtils'
+
+const getDeliverySlots = (): string[] => {
+  const slots: string[] = []
+  const currentDate = new Date()
+  const oneMonthLater = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    currentDate.getDate()
+  )
+
+  for (
+    let date = new Date(currentDate);
+    // eslint-disable-next-line no-unmodified-loop-condition
+    date <= oneMonthLater;
+    date.setDate(date.getDate() + 1)
+  ) {
+    // Check if the day is Sunday(0) or Monday (1)
+    if (date.getDay() === 0 || date.getDay() === 1) {
+      const slot = formatDate(date.toISOString()) // Format the date as Day, Month Num
+      slots.push(slot)
+    }
+  }
+
+  return slots
+}
+
+interface Basket {
+  items: []
+  deliveryDate: string
+}
+
+export { getDeliverySlots, type Basket }
