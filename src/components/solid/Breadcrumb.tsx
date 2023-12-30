c717200cd3863890ad8e6a4047e79c3b50189a24
Chening Duker <cdduker@gmail.com>
2023-12-30 20:51:43 +0000

    Revert "Sidebar Menu and Breadcrumb Functionality"

--- src/components/solid/Breadcrumb.tsx
+++ src/components/solid/Breadcrumb.tsx
@@ -1,54 +1,28 @@
-import { Show, For } from "solid-js";
+import { For } from "solid-js";
 
 const Breadcrumb = (props) => {
-  const { categories, collectionId } = props;
+  const { collections, currentCollectionId } = props;
 
-  let checkCategory = categories.find((col) => col.id === collectionId);
-  let categoryId = checkCategory.parent_id
-    ? collectionId
-    : categories.filter((col) => col.parent_id === collectionId)[0].id;
+  const findParentPath = (collectionId) => {
+    let path = [];
+    let current = collections.find((col) => col.id === collectionId);
 
-  let categoryHasChild =
-    categories.filter((col) => col.parent_id === checkCategory.id).length > 0
-      ? categories.filter((col) => col.parent_id === checkCategory.id)[0]
-      : null;
+    while (current) {
+      path.unshift(current);
+      current = collections.find((col) => col.id === current.parent_id);
+    }
 
-  if (categoryHasChild) {
-    categoryId = categoryHasChild.id;
-  }
+    return path;
+  };
 
-  let currentCategory = categories.find((col) => col.id === categoryId);
-  let parentCategory = categories.find(
-    (col) => col.id === currentCategory.parent_id,
-  );
-
-  let breadcrumb = [];
-  breadcrumb.push(currentCategory);
-
-  if (parentCategory && parentCategory.parent_id) {
-    breadcrumb.unshift(parentCategory);
-  }
-
-  let categoryParentsCount = 0;
-  let topLevelCategory = parentCategory ? parentCategory : currentCategory;
-
-  while (parentCategory) {
-    categoryParentsCount++;
-    topLevelCategory = parentCategory;
-    parentCategory = categories.find(
-      (col) => col.id === parentCategory.parent_id,
-    );
-  }
-
-  if (categoryParentsCount === 0) {
-  }
+  const parentPath = findParentPath(currentCollectionId);
 
   const websiteDomain = import.meta.env.VITE_WEBSITE_DOMAIN;
 
   const breadcrumbList = {
     "@context": "https://schema.org",
     "@type": "BreadcrumbList",
-    itemListElement: breadcrumb.map((collection, index) => ({
+    itemListElement: parentPath.map((collection, index) => ({
       "@type": "ListItem",
       position: index + 1,
       item: {
@@ -64,56 +38,40 @@ const Breadcrumb = (props) => {
         class="flex items-center whitespace-nowrap w-full max-w-7xl mx-auto p-4"
         aria-label="Breadcrumb"
       >
-        <li class="flex md:hidden">
+        <li class="inline-flex items-center">
           <a
             class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500"
-            href={`/market/${topLevelCategory.slug}`}
+            href="/market"
           >
-            {topLevelCategory.name}
+            Home
           </a>
-          <svg
-            class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600 dark:text-neutral-600"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="m9 18 6-6-6-6" />
-          </svg>
         </li>
-        <For each={breadcrumb} fallback={<div>Error loading breadcrumb</div>}>
+        <For each={parentPath} fallback={<div>Loading...</div>}>
           {(collection, i) => (
             <>
               <li
                 class={
-                  collection.id !== categoryId
+                  collection.id !== currentCollectionId
                     ? "inline-flex items-center"
                     : "inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-gray-200"
                 }
               >
-                <Show when={i() > 0}>
-                  <svg
-                    class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600 dark:text-neutral-600"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="m9 18 6-6-6-6" />
-                  </svg>
-                </Show>
+                <svg
+                  class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600 dark:text-neutral-600"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="m9 18 6-6-6-6" />
+                </svg>
 
-                {collection.id !== categoryId ? (
+                {collection.id !== currentCollectionId ? (
                   <a
                     class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500"
                     href={`/market/${collection.slug}`}
--- src/components/solid/global/Header.tsx
+++ src/components/solid/global/Header.tsx
@@ -1,14 +1,30 @@
+import { useStore } from "@nanostores/solid";
 import { createSignal, Show, For, type Component } from "solid-js";
 import logo from "@assets/logo.png";
-import Sidebar from "@components/solid/global/Sidebar.tsx";
+import { isCartOpen, isMenuOpen } from "../../../store.js";
 
 interface IProps {
   collections: any;
 }
 
 const Header: Component<IProps> = ({ collections }) => {
+  // read the store value with the `useStore` hook
+  const $isCartOpen = useStore(isCartOpen);
+  const $isMenuOpen = useStore(isMenuOpen);
   const [isSearchOpen, setSearchOpen] = createSignal(false);
+
   const toggleSearch = () => setSearchOpen(!isSearchOpen());
+
+  const categories = collections.filter((col) => col.parent_id == null);
+
+  function getSubCategories(parentId) {
+    const categories = collections.filter((col) => col.parent_id == parentId);
+
+    if (categories) {
+      return categories;
+    }
+    return [];
+  }
 
   return (
     <>
@@ -108,7 +124,50 @@ const Header: Component<IProps> = ({ collections }) => {
                 </div>
               }
             >
-              <Sidebar collections={collections} />
+              <div
+                id="navbar-collapse-with-animation"
+                class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow lg:flex lg:justify-center"
+              >
+                <div class="flex flex-col gap-x-0 mt-5 divide-y divide-dashed divide-gray-200 lg:flex-row lg:items-center lg:justify-end lg:gap-x-7 lg:mt-0 lg:ps-7 lg:divide-y-0 lg:divide-solid dark:divide-gray-700">
+                  <For each={categories}>
+                    {(collection, i) => (
+                      <Show
+                        when={getSubCategories(collection.id).length == 0}
+                        fallback={
+                          <div class="hs-dropdown [--strategy:static] md:[--strategy:absolute] [--adaptive:none] md:[--trigger:hover] py-3 md:py-6">
+                            <a
+                              href={`/market/${collection.slug}`}
+                              class="flex items-center w-full text-gray-800 hover:text-gray-600 font-medium dark:text-gray-200 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                            >
+                              {collection.name}
+                            </a>
+
+                            <div class="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] md:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 md:w-80 hidden z-10 bg-white md:shadow-2xl rounded-lg py-2 md:p-2 dark:bg-gray-800 dark:divide-gray-700 before:absolute top-full before:-top-5 before:start-0 before:w-full before:h-5">
+                              <For each={getSubCategories(collection.id)}>
+                                {(subCollection, i) => (
+                                  <a
+                                    class="inline-flex gap-x-5 w-full p-4 text-gray-600 rounded-lg hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                                    href={`/market/${subCollection.slug}`}
+                                  >
+                                    {subCollection.name}
+                                  </a>
+                                )}
+                              </For>
+                            </div>
+                          </div>
+                        }
+                      >
+                        <a
+                          class="font-medium text-gray-500 hover:text-gray-400 py-3 lg:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                          href={`/market/${collection.slug}`}
+                        >
+                          {collection.name}
+                        </a>
+                      </Show>
+                    )}
+                  </For>
+                </div>
+              </div>
             </Show>
 
             <div class="hidden lg:flex items-center gap-x-4">
