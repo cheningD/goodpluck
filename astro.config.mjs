f93532f55f8d7412217ff09ff50106623872b785
Chening Duker <cdduker@gmail.com>
2024-01-06 15:06:15 -0500

    Deploy to vercel instead of cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
-import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solid(), tailwind()],
   output: "server",
-  adapter: cloudflare({
-    runtime: {
-      mode: "local",
-      type: "pages",
-    },
-  }),
+  adapter: vercel(),
 });
