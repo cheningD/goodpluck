7fe918829420e3fbfb81f726de2c7eb47a3f1612
Chening Duker <cdduker@gmail.com>
2024-01-06 16:48:15 -0500

    fix naming SWELL_STORE_ID

--- src/lib/swell.ts
+++ src/lib/swell.ts
@@ -1,7 +1,7 @@
 import swellnode from "swell-node";
 
 const swell = swellnode.init(
-  import.meta.env.PUBLIC_SWELL_STORE_ID,
+  import.meta.env.SWELL_STORE_ID,
   import.meta.env.SWELL_SECRET_KEY,
 );
 
