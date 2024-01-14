dec9f1342c5fc1967433f96ef679ad5002a92b83
Tarik <52907282+wpplumber@users.noreply.github.com>
2024-01-14 18:12:33 +0100

    Search Button UI Component (#80)
    * (WIP) Search Button UI Component
    
    * Omit Wrangler's build and test Astro build on Cloudflare
    
    * Meaningful error messages and accept formatting with double quotes..
    
    * Fix minor issues after rebase
    
    * Avoid astro build check  JavaScript heap out of memory and upgrade wrangler
    
    * Use swell-js beside swell-node until review
    
    * Fix TS error
    
    * Vercel doesn't read the import.meta.env
    
    * Prioritize process.env
    
    * Output env variables to check the values on Vercel preview
    
    * Revert to import.meta.env after confirmation that Vercel uses it and return the correct values
    
    * Fix Search problems and bypass E2E
    
    * Get Playwright tests all passed locally

---
+++ src/components/solid/AlgoliaSearch.tsx
@@ -0,0 +1,140 @@
+import { createSignal, createEffect, type Component } from "solid-js";
+import algoliasearch from "algoliasearch/lite";
+import { setIsSearchVisible } from "../../store";
+
+const searchClient = algoliasearch(
+  import.meta.env.PUBLIC_ALGOLIA_APPLICATION_ID,
+  import.meta.env.PUBLIC_ALGOLIA_API_KEY,
+);
+const index = searchClient.initIndex(import.meta.env.PUBLIC_ALGOLIA_INDEX_NAME);
+
+const AlgoliaSearch: Component = () => {
+  const [query, setQuery] = createSignal("");
+  const [hits, setHits] = createSignal<any>([]);
+  const [debouncedQuery, setDebouncedQuery] = createSignal(query());
+  const [, setIsLoading] = createSignal(false);
+  const [, setError] = createSignal(null);
+  const [isUserInteracted, setIsUserInteracted] = createSignal(false);
+  const [isDialogVisible, setIsDialogVisible] = createSignal(false);
+  const hideSearch = (): void => {
+    setQuery("");
+    setIsUserInteracted(false);
+    setIsDialogVisible(false);
+    setIsSearchVisible(false);
+  };
+
+  const performSearch = async (): Promise<void> => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const { hits } = await index.search(query());
+      setHits(hits);
+    } catch (err: any) {
+      setError(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // Debounce logic
+  createEffect(() => {
+    const handler = setTimeout(() => {
+      if (isUserInteracted()) {
+        setDebouncedQuery(query());
+      }
+    }, 300);
+    return () => {
+      clearTimeout(handler);
+    };
+  });
+
+  // Search effect
+  createEffect(() => {
+    if (debouncedQuery()) {
+      void performSearch();
+    }
+  });
+
+  const navIcon =
+    '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M448 64L64 240.14h200a8 8 0 0 1 8 8V448Z"/></svg>';
+
+  return (
+    <>
+      <div class="relative py-2 px-4 w-full">
+        <div class="flex items-center gap-x-4">
+          <label class="hidden lg:flex" for="search-input">
+            Search:
+          </label>
+          <div class="relative w-full">
+            <div class="relative flex items-center justify-between gap-x-1">
+              <input
+                id="search-input"
+                type="text"
+                class="bg-opacity-50 opacity-100 transition-opacity duration-300 py-3 px-5 block w-10/12 lg:w-full border-gray-200 rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
+                value={query()}
+                onInput={(e) => {
+                  setIsDialogVisible(true);
+                  setIsUserInteracted(true);
+                  setQuery(e.currentTarget.value);
+                  void performSearch();
+                }}
+                placeholder="What are you looking for Today?"
+              />
+
+              <svg
+                onClick={hideSearch}
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-8 h-8 cursor-pointer"
+                viewBox="0 0 32 32"
+              >
+                <path
+                  fill="currentColor"
+                  d="M16 2C8.2 2 2 8.2 2 16s6.2 14 14 14s14-6.2 14-14S23.8 2 16 2zm5.4 21L16 17.6L10.6 23L9 21.4l5.4-5.4L9 10.6L10.6 9l5.4 5.4L21.4 9l1.6 1.6l-5.4 5.4l5.4 5.4l-1.6 1.6z"
+                />
+              </svg>
+            </div>
+
+            {isDialogVisible() && (
+              <div class="absolute mt-14 inset-0 flex justify-center items-start w-full h-[70vh]  overflow-x-hidden overflow-y-auto">
+                <div class="bg-slate-200 p-6 shadow-md w-full mx-auto flex flex-col">
+                  {hits().length === 0 ? (
+                    <>
+                      <p class="font-semibold text-orange-600">
+                        Sorry, we couldn't find{" "}
+                        <span class="font-medium text-black">{query()}</span>
+                      </p>
+                      <p class="font-semibold text-orange-600">
+                        but our support would love to help you find what you're
+                        hunting for.
+                      </p>
+
+                      <ul class="list-inside list-disc">
+                        <li>
+                          Check your search term for typos or misspellings.
+                        </li>
+                        <li>Chat with a friendly customer service.</li>
+                      </ul>
+                    </>
+                  ) : (
+                    <ul class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+                      <li class="text-yellow-500 hidden"></li>
+                      {hits().map((hit: any) => (
+                        <li
+                          class="inline-flex items-center justify-between gap-x-2 py-3 px-4 text-sm font-medium odd:bg-gray-100 bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg dark:odd:bg-slate-800 dark:bg-slate-900 dark:border-gray-700 dark:text-white"
+                          innerHTML={`<a class="flex w-full justify-between items-center" href="/product/${hit.Slug}"><div class="flex justify-between gap-x-1">${hit._highlightResult.Name.value}</div> ${navIcon}</a>`}
+                        ></li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AlgoliaSearch;
--- src/components/solid/Products.tsx
+++ src/components/solid/Products.tsx
@@ -1,51 +1,77 @@
-import { type Component, Show, createSignal } from "solid-js";
+import { Show, type Component, createSignal, onCleanup } from "solid-js";
+import { initSwell } from "../../lib/swell-js";
+import { throttle } from "../../lib/throttle";
 
-// import { swell } from "../../lib/swell";
-
-interface iProps {
-  currentCategory: any;
+interface IProps {
+  currentCategory: string | undefined;
 }
+const swell = initSwell(
+  import.meta.env.PUBLIC_SWELL_STORE_ID,
+  import.meta.env.PUBLIC_SWELL_PUBLIC_KEY,
+);
 
-const Products: Component<iProps> = (props) => {
-  const [products] = createSignal([]);
-  const [isLoading] = createSignal(false);
-  // const [page, setPage] = createSignal(1);
-  const [totalProducts] = createSignal(122); // total number of products in Swell currently
-  // let isFetching = false; // flag to indicate if fetching is in progress
+const Products: Component<IProps> = ({ currentCategory }) => {
+  const [products, setProducts] = createSignal([]);
+  const [isLoading, setIsLoading] = createSignal(false);
+  const [page, setPage] = createSignal(1);
+  const [totalProducts, setTotalProducts] = createSignal(0);
+  setTotalProducts(122); // total number of products in Swell currently
+  let isFetching = false; // flag to indicate if fetching is in progress
+  const [error, setError] = createSignal(false);
+  const [errorMsg, setErrorMsg] = createSignal("");
 
-  // const fetchProducts: () => Promise<void> = async () => {
-  //   if (products().length >= totalProducts() || isFetching) {
-  //     return;
-  //   }
+  const fetchProducts = async (): Promise<void> => {
+    if (products().length >= totalProducts() || isFetching) {
+      return;
+    }
 
-  //   isFetching = true;
-  //   setIsLoading(true);
-  //   const newProducts = await swell.products.list({
-  //     limit: 10,
-  //     page: page(),
-  //   });
+    isFetching = true;
+    setError(false);
+    setErrorMsg("");
+    setIsLoading(true);
 
-  //   setProducts([...products(), ...newProducts.results]);
-  //   isFetching = false;
-  //   setIsLoading(false);
-  //   setPage(page() + 1);
-  // };
+    try {
+      let newProducts = [];
+      newProducts.results = [];
+      if (currentCategory !== "") {
+        newProducts = await swell.products.list({
+          category: `${currentCategory}`, // Slug or ID
+          limit: 10,
+          page: page(),
+        });
+      } else {
+        newProducts = await swell.products.list({
+          limit: 10,
+          page: page(),
+        });
+      }
 
-  // const checkScroll: () => Promise<void> = async () => {
-  //   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-  //     await fetchProducts();
-  //   }
-  // };
+      setProducts([...products(), ...newProducts.results]);
+      setPage(page() + 1);
+    } catch (err) {
+      setError(true);
+      setErrorMsg("Failed to load products. Try again later.");
+    }
+
+    isFetching = false;
+    setIsLoading(false);
+  };
+
+  const checkScroll = async (): Promise<void> => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      await fetchProducts();
+    }
+  };
 
   // Throttle the scroll event to prevent rapid firing
-  // const throttledCheckScroll = throttle(checkScroll, 200);
+  const throttledCheckScroll = throttle(checkScroll, 200);
 
-  // window.addEventListener("scroll", throttledCheckScroll);
-  // await fetchProducts();
+  window.addEventListener("scroll", throttledCheckScroll);
+  void fetchProducts();
 
-  // onCleanup(() => {
-  //   window.removeEventListener("scroll", throttledCheckScroll);
-  // });
+  onCleanup(() => {
+    window.removeEventListener("scroll", throttledCheckScroll);
+  });
 
   const itemListElement = products().map((product, index) => ({
     "@type": "ListItem",
@@ -64,47 +90,79 @@ const Products: Component<iProps> = (props) => {
 
   return (
     <>
-      <span class="text-4xl text-blue-600">Products:</span>
+      <span class="text-4xl text-blue-600" data-testid="product-list">
+        Products:
+      </span>
       <div>
-        <ul class="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-10 justify-center">
-          {products().map((product: any) => (
-            <li class="flex flex-col gap-y-2">
-              <div class="relative rounded-xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-52">
-                <a href={`/product/${product.slug}`}>
-                  <Show when={product.images !== undefined}>
-                    <img
-                      alt={`Image of ${product.name}`}
-                      src={product.images[0].file.url}
-                      width="305"
-                      height="205"
-                      loading="lazy"
-                      decoding="async"
-                      class="absolute w-full h-full rounded-xl"
-                    />
-                  </Show>
-                  <button
-                    aria-label="Add to Cart"
-                    type="button"
-                    class="absolute bottom-2 right-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border-2 border-gray-900 text-white bg-gray-800 shadow-sm hover:bg-transparent disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                  >
-                    + Quick add
-                  </button>
-                </a>
-              </div>
-              <h2 class="text-xl font-serif" href={`/product/${product.slug}`}>
-                <span class="hidden">Product Name:</span>
-                {product.name}
-              </h2>
-              <div class="flex justify-between items-center">
-                <span class="text-xs text-gray-600">{product.kind}</span>
-                <span class="text-right font-semibold">${product.price}</span>
-              </div>
-            </li>
-          ))}
-        </ul>
-        {isLoading() && <div>Loading more products...</div>}
+        {error() && <div data-testid="product-error">Error:{errorMsg()}</div>}
+        <Show when={products().length > 0}>
+          <ul
+            data-testid="product-items"
+            class="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-10 justify-center"
+          >
+            {products().map((product: any) => (
+              <li class="flex flex-col gap-y-2">
+                <div class="relative rounded-xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-52">
+                  <a href={`/product/${product.slug}`}>
+                    <Show when={product.images !== undefined}>
+                      <img
+                        alt={`Image of ${product.name}`}
+                        src={product.images[0].file.url}
+                        width="305"
+                        height="205"
+                        loading="lazy"
+                        decoding="async"
+                        class="absolute w-full h-full rounded-xl"
+                      />
+                    </Show>
+                    <button
+                      aria-label="Add to Cart"
+                      type="button"
+                      class="absolute bottom-2 right-2 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border-2 border-gray-900 text-white bg-gray-800 shadow-sm hover:bg-transparent disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                    >
+                      + Quick add
+                    </button>
+                  </a>
+                </div>
+                <h2
+                  class="text-xl font-serif"
+                  href={`/product/${product.slug}`}
+                >
+                  <span class="hidden">Product Name:</span>
+                  {product.name}
+                </h2>
+                <div class="flex justify-between items-center">
+                  <span class="text-xs text-gray-600">{product.kind}</span>
+                  <span class="text-right font-semibold">${product.price}</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Show>
+        {isLoading() && (
+          <div class="text-red-500">Loading more products...</div>
+        )}
         {products().length >= totalProducts() && (
           <div>You've reached the end of products...</div>
+        )}
+        {products().length === 0 && (
+          <div data-testid="no-products">
+            No product found in this category!
+          </div>
+        )}
+        {error() && (
+          <button
+            data-testid="retry-fetch"
+            onClick={() => {
+              fetchProducts().catch((error) => {
+                // Handle any errors that occur during fetchProducts
+                setError(true);
+                setErrorMsg(error);
+              });
+            }}
+          >
+            Retry
+          </button>
         )}
       </div>
 
