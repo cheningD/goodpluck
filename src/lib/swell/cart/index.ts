a1f3cb054ca57d824673be549be6d69fd76c2c52
“ashra-f” <ashrafhammoud2@gmail.com>
2024-01-14 21:57:24 -0500

    Refactor cart creation and retrieval logic

--- src/lib/swell/cart/index.ts
+++ src/lib/swell/cart/index.ts
@@ -1,6 +1,7 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { stytch } from "src/lib/stytch";
 import { swell } from "..";
 import calculateCartDates from "./dates";
+import type { Account, Cart } from "swell-js";
 
 const {
   orderingWindowStartDate,
@@ -9,7 +10,13 @@ const {
   deliveryDate,
 } = calculateCartDates();
 
-const fetchActiveCart = async (account: any) => {
+/**
+ * Get the active cart for the account
+ * @param account - the Swell account
+ * @returns Cart or null if it fails to retrieve the cart
+ * @throws Error when it fails to retrieve the cart
+ */
+const getCart = async (account: Account): Promise<Cart | null> => {
   try {
     const cart = await swell.get("/carts/", {
       where: {
@@ -30,30 +37,63 @@ const fetchActiveCart = async (account: any) => {
     }
   } catch (error) {
     console.error(error);
-    throw error;
+    return null;
   }
 };
 
-const fetchAllCarts = async (account: any) => {
+/**
+ * Get the active cart for the account from the session token
+ * @param sessionToken - the session token from Stytch
+ * @returns Cart or null if it fails to retrieve the cart
+ * @throws Error when it fails to retrieve the cart
+ */
+const getCartFromSession = async (
+  sessionToken: string,
+): Promise<Cart | null> => {
   try {
-    return swell.get("/carts/", {
-      where: { account_id: account.id },
+    const session = await stytch.sessions.authenticate({
+      session_token: sessionToken,
     });
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
-const fetchOrCreateCart = async (account: any) => {
-  try {
-    const activeCart = await fetchActiveCart(account);
-    if (activeCart) {
-      console.log("Active cart already exists");
-      return activeCart;
+    if (!session) {
+      throw new Error("Failed to retrieve user session information.");
     }
 
-    return swell.post("/carts", {
+    const userEmail = session.user.emails?.[0]?.email;
+    if (!userEmail) {
+      throw new Error("User email is not available in session.");
+    }
+
+    const account = await swell.get(`/accounts/${userEmail}`);
+    if (!account) {
+      throw new Error("Failed to retrieve user account information.");
+    }
+
+    const cart = await getCart(account);
+    if (!cart) {
+      throw new Error("Failed to fetch or create a cart.");
+    }
+
+    return cart;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error occurred: ", error.message);
+    } else {
+      console.error("Error occurred: ", error);
+    }
+    return null;
+  }
+};
+
+/**
+ * Create a new cart for the account
+ * @param account - the Swell account
+ * @returns Cart or null if it fails to create the cart
+ * @throws Error when it fails to create the cart
+ */
+const createCart = async (account: Account): Promise<Cart | null> => {
+  try {
+    console.log("Creating a new cart for the account");
+    const newCart = await swell.post("/carts", {
       account_id: account.id,
       shipping: account.shipping,
       currency: "USD",
@@ -64,10 +104,33 @@ const fetchOrCreateCart = async (account: any) => {
       order_charge_date: orderChargeDate,
       delivery_date: deliveryDate,
     });
+
+    return newCart;
   } catch (error) {
-    console.error(error);
-    throw error;
+    console.error("Error in createCart:", error);
+    return null;
   }
 };
 
-export { fetchAllCarts, fetchActiveCart, fetchOrCreateCart };
+/**
+ * Get the active cart for the account or create a new one if it doesn't exist
+ * @param account - the Swell account
+ * @returns Cart or null if it fails to retrieve or create the cart
+ * @throws Error when it fails to retrieve or create the cart
+ */
+const getOrCreateCart = async (account: Account): Promise<Cart | null> => {
+  try {
+    const activeCart = await getCart(account);
+    if (activeCart) {
+      console.log("Active cart already exists");
+      return activeCart;
+    }
+
+    return await createCart(account);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export { getOrCreateCart, getCart, createCart, getCartFromSession };
