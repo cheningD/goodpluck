a2b01530f80aa0cce4718aa3681965b7f5a63bea
“ashra-f” <ashrafhammoud2@gmail.com>
2024-01-18 20:19:53 -0500

    duplicate code

--- src/components/solid/Cart.tsx
+++ src/components/solid/Cart.tsx
@@ -1,6 +1,5 @@
 import type { Cart as CartType } from "swell-js";
 import type { Component } from "solid-js";
-import type { Cart as CartType } from "swell-js";
 
 interface CartProps {
   cart: CartType | null;
