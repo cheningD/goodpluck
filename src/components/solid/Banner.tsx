a223fdd18a281cc8581e64a92d50f6ed8031f4c6
Chening Duker <cdduker@gmail.com>
2024-01-24 18:22:50 -0500

    improve edit icon and simplify

--- src/components/solid/Banner.tsx
+++ src/components/solid/Banner.tsx
@@ -1,38 +1,43 @@
 import { useStore } from "@nanostores/solid";
 import { isCartOpen } from "../../store";
 import type { Component } from "solid-js";
-
-/* If no zip is found, prompt user for zip, else show delivery date */
+import { EditIcon } from "./Icons";
 
 interface BannerProps {
   isZipDeliverable: boolean;
   deliveryDate: string;
 }
+
 const Banner: Component<BannerProps> = ({ isZipDeliverable, deliveryDate }) => {
   const $isCartOpen = useStore(isCartOpen);
 
+  const displayMessage = (): string => {
+    if (!isZipDeliverable) {
+      return "Free Delivery to Detroit & Nearby.";
+    }
+    return deliveryDate
+      ? `Shopping for ${deliveryDate}`
+      : `Choose a delivery date`;
+  };
+
+  const handleButtonClick = (): void => {
+    isCartOpen.set(!$isCartOpen());
+  };
+
   return (
-    <div class="bg-brand-green text-white text-sm p-2">
-      <span>
-        {isZipDeliverable
-          ? deliveryDate
-            ? `Shopping for ${deliveryDate}`
-            : `Choose a delivery date`
-          : "Free Delivery to Detroit & Nearby."}
-        <button
-          class="underline hover:no-underline hover:cursor-pointer px-2"
-          data-hs-overlay-backdrop-container="#sidebar-page"
-          data-hs-overlay="#sidebar-mini"
-          aria-controls="sidebar-mini"
-          aria-label="Toggle navigation"
-          onClick={() => {
-            isCartOpen.set(!$isCartOpen());
-            console.log(`Why no open ${$isCartOpen()}`);
-          }}
-        >
-          {isZipDeliverable ? "Edit" : "Check your zip"}
-        </button>
-      </span>
+    <div class="flex items-center bg-brand-green text-white text-sm p-2">
+      <span>{displayMessage()}</span>
+      <button
+        class="flex items-center underline hover:font-semibold hover:cursor-pointer px-2 gap-2"
+        data-hs-overlay-backdrop-container="#sidebar-page"
+        data-hs-overlay="#sidebar-mini"
+        aria-controls="sidebar-mini"
+        aria-label="Toggle navigation"
+        onClick={handleButtonClick}
+      >
+        {isZipDeliverable ? "Edit" : "Check your zip"}
+        {EditIcon}
+      </button>
     </div>
   );
 };
---
+++ src/components/solid/Icons.tsx
@@ -0,0 +1,34 @@
+// Icons
+export const HamburgerIcon = (
+  <svg class="w-6 h-6" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M3 6h18v2H3zm0 5h18v2H3zm0 5h18v2H3z" />
+  </svg>
+);
+export const SearchIcon = (
+  <svg class="w-6 h-6 cursor-pointer" viewBox="0 0 24 24">
+    <path
+      d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5l-1.5 1.5l-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16A6.5 6.5 0 0 1 3 9.5A6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14S14 12 14 9.5S12 5 9.5 5Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+export const CartIcon = "Cart"; // Replace with actual icon
+export const AccountIcon = <a href="/account">Account</a>;
+
+export const EditIcon = (
+  <svg
+    class="feather feather-edit"
+    fill="none"
+    height="18"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    viewBox="0 0 24 24"
+    width="18"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+  </svg>
+);
