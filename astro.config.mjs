fcaeb611993f15463f1a3a53710fb3b3a6486c63
“ashra-f” <ashrafhammoud2@gmail.com>
2024-03-22 12:36:20 -0400

    add sentry only in production

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,25 @@ import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import sentry from "@sentry/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), tailwind()],
+  integrations: [
+    solid(),
+    tailwind(),
+    ...(process.env.SENTRY_AUTH_TOKEN
+      ? [
+          sentry({
+            dsn: "https://b6e371214433705eb55c814b184fba23@o4506955454152704.ingest.us.sentry.io/4506955579457536",
+            sourceMapsUploadOptions: {
+              project: "goodpluck",
+              authToken: process.env.SENTRY_AUTH_TOKEN,
+            },
+          }),
+        ]
+      : []),
+  ],
   output: "server",
   adapter: vercel(),
 });
