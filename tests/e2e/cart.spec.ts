20b7f3fb4bce920bc99fe2575ee2b771d95e0f00
Chening Duker <cdduker@gmail.com>
2024-03-27 18:04:57 -0400

    Add zipcode checks to the cart test

--- tests/e2e/cart.spec.ts
+++ tests/e2e/cart.spec.ts
@@ -1,12 +1,29 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("New Cart", () => {
-  test("should display a zip input on creation", async ({ page }) => {
+  test("should display a zip input on creation and show an error message for unsupported zip codes", async ({
+    page,
+  }) => {
     await page.goto("/");
     await page.waitForLoadState("networkidle");
     await page.getByTestId("cart-btn").click();
     await page.waitForTimeout(400);
     await expect(page.getByTestId("zip-input")).toBeVisible();
     await expect(page.getByTestId("zip-input")).toBeFocused();
+
+    // Input an invalid zip
+    await page.getByTestId("zip-input").fill("10120");
+    await page.getByTestId("zip-submit-btn").click();
+    await expect(
+      page.getByText("Sorry, we don't serve 10120 yet!"),
+    ).toBeVisible();
+
+    // Try a new Zip
+    await page.getByTestId("try-different-zip").click();
+    await expect(page.getByTestId("zip-input")).toBeVisible();
+    await page.getByTestId("zip-input").fill("48206");
+    await page.getByTestId("zip-submit-btn").click();
+    await expect(page.getByText("Add items to your basket!")).toBeVisible();
+    //
   });
 });
