956d8b5b91594cd0c51b6c5cf437b4b7c101690e
“ashra-f” <ashrafhammoud2@gmail.com>
2024-04-10 00:03:27 -0400

    manage delivery date

---
+++ src/components/solid/SubscriptionCard.tsx
@@ -0,0 +1,17 @@
+import { type Component } from "solid-js";
+import { DeliveryDateSelector } from "./DeliveryDateSelector";
+
+export const SubscriptionCard: Component = () => {
+  return (
+    <div class="subscription-card-container border border-brand-green p-4 m-4">
+      <div class="flex items-center">
+        <p class="text-lg">Subscription</p>
+        <button class="text-brand-green ml-2 text-sm">
+          Skip upcoming basket
+        </button>
+      </div>
+
+      <DeliveryDateSelector />
+    </div>
+  );
+};
