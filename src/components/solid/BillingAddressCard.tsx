c75a7cf882cb72cb9f4b98b3a3ae31473f1d3bf5
“ashra-f” <ashrafhammoud2@gmail.com>
2024-05-19 19:06:42 -0400

    fix billing info refetch

--- src/components/solid/BillingAddressCard.tsx
+++ src/components/solid/BillingAddressCard.tsx
@@ -3,16 +3,17 @@ import { createSignal, Show, type Component } from "solid-js";
 import { $swellAccount } from "src/lib/store";
 import { BillingAddressEditor } from "./BillingAddressEditor";
 import { BillingAddressDisplay } from "./BillingAddressDisplay";
+import type { Billing } from "swell-js";
 
 export const BillingAddressCard: Component = () => {
   const [editing, setEditing] = createSignal(false);
   const account = useStore($swellAccount);
-  const billing = account()?.billing;
+  const getBilling = (): Billing | undefined => account()?.billing;
 
   return (
     <div class="billing-address-container border border-brand-green p-4 m-4">
       <h3 class="text-lg">Billing Address</h3>
-      <Show when={billing} fallback={<p>No billing info found.</p>}>
+      <Show when={getBilling()} fallback={<p>No billing info found.</p>}>
         <Show when={editing()} fallback={<BillingAddressDisplay />}>
           <BillingAddressEditor setEditing={setEditing} />
         </Show>
--- src/components/solid/BillingInfoCard.tsx
+++ src/components/solid/BillingInfoCard.tsx
@@ -3,16 +3,18 @@ import { createSignal, Show, type Component } from "solid-js";
 import { $swellAccount } from "src/lib/store";
 import { BillingCardDisplay } from "./BillingCardDisplay";
 import { BillingCardEditor } from "./BillingCardEditor";
+import type { Card } from "swell-js";
 
 export const BillingInfoCard: Component = () => {
   const [editing, setEditing] = createSignal(false);
   const account = useStore($swellAccount);
-  const billingCard = account()?.billing?.card;
+  const getBillingCard = (): Omit<Card, "billing"> | undefined =>
+    account()?.billing?.card;
 
   return (
     <div class="billing-card-container border border-brand-green p-4 m-4">
       <h3 class="text-lg">Card Details</h3>
-      <Show when={billingCard} fallback={<p>No card found.</p>}>
+      <Show when={getBillingCard()} fallback={<p>No card found.</p>}>
         <Show when={editing()} fallback={<BillingCardDisplay />}>
           <BillingCardEditor setEditing={setEditing} />
         </Show>
