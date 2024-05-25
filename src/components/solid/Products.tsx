fec0bcaa6c185a608cc9af361b5acd2187d7a796
“ashra-f” <ashrafhammoud2@gmail.com>
2024-05-25 14:00:31 -0400

    Add ability to filter products using search bar

--- src/components/solid/Products.tsx
+++ src/components/solid/Products.tsx
@@ -1,11 +1,26 @@
 import type { GoodpluckCategory, GoodpluckProduct } from "src/lib/types";
-import { For, type Component, onMount } from "solid-js";
+import {
+  For,
+  type Component,
+  createSignal,
+  onMount,
+  Show,
+  createMemo,
+} from "solid-js";
+import Fuse from "fuse.js";
 import Product from "./Product";
+import { SearchBar } from "./SearchBar";
 
 interface IProps {
-  currentCategory?: GoodpluckCategory | undefined;
+  currentCategory?: GoodpluckCategory;
   categories: GoodpluckCategory[];
   productMap: Map<string, GoodpluckProduct>;
+}
+
+interface FilteredCategory {
+  name: string;
+  slug: string;
+  products: GoodpluckProduct[];
 }
 
 const Products: Component<IProps> = ({
@@ -13,48 +28,101 @@ const Products: Component<IProps> = ({
   categories,
   productMap,
 }) => {
+  const [searchTerm, setSearchTerm] = createSignal("");
+
   onMount(() => {
-    if (!currentCategory) {
-      return;
-    }
     // Scroll to the currentCategory
-    const hash = `#${currentCategory.slug}`;
-    if (hash) {
-      console.log("hash", hash);
-      const target = document.querySelector(hash);
+    if (currentCategory) {
+      const target = document.querySelector(`#${currentCategory.slug}`);
       if (target) {
         const offset =
-          target.getBoundingClientRect().top + window.scrollY - 170; // Adjust the offset value (80) based on your navbar's height
-        window.scrollTo({
-          top: offset,
-          behavior: "smooth",
-        });
+          target.getBoundingClientRect().top + window.scrollY - 170; // Adjust the offset value (170) based on your navbar's height
+        window.scrollTo({ top: offset, behavior: "smooth" });
       }
     }
   });
 
+  const fuse = createMemo(() => {
+    const products = categories.flatMap((category) =>
+      category.products.results
+        .map((p) => productMap.get(p.product_id))
+        .filter((product): product is GoodpluckProduct => product !== undefined)
+        .map((product) => ({ ...product, category: category.name })),
+    );
+
+    return new Fuse(products, {
+      includeScore: false,
+      threshold: 0.2, // the lower the value, the more accurate the results
+      shouldSort: false, // whether to sort the result list by score
+      keys: ["name", "category"],
+    });
+  });
+
+  const getFilteredCategories = createMemo((): FilteredCategory[] => {
+    const term = searchTerm().toLowerCase();
+    if (!term) {
+      return categories.map((category) => ({
+        name: category.name,
+        slug: category.slug,
+        products: category.products.results
+          .map((p) => productMap.get(p.product_id))
+          .filter(
+            (product): product is GoodpluckProduct => product !== undefined,
+          ),
+      }));
+    }
+
+    const results = fuse().search(term);
+    const filteredProducts = results.map((result) => result.item);
+
+    const categoryMap = new Map<string, FilteredCategory>();
+
+    filteredProducts.forEach((product) => {
+      if (!categoryMap.has(product.category)) {
+        const category = categories.find(
+          (cat) => cat.name === product.category,
+        );
+        if (category) {
+          categoryMap.set(product.category, {
+            name: category.name,
+            slug: category.slug,
+            products: [],
+          });
+        }
+      }
+      categoryMap.get(product.category)?.products.push(product);
+    });
+
+    return Array.from(categoryMap.values());
+  });
+
   return (
     <div>
-      <For each={categories}>
-        {(category) => (
-          <div class="mb-8">
-            <h1 class="text-2xl font-bold mb-4 scroll-mt-20" id={category.slug}>
-              {category.name}
-            </h1>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              <For each={category.products.results}>
-                {(p) => {
-                  const product = productMap.get(p.product_id);
-                  if (!product) {
-                    return null;
-                  }
-                  return <Product product={product} />;
-                }}
-              </For>
-            </div>
+      <SearchBar onSearch={setSearchTerm} />
+      <Show
+        when={getFilteredCategories().length > 0}
+        fallback={
+          <div class="text-center my-8">
+            <h2 class="text-xl font-semibold">No products found.</h2>
+            <p>Try adjusting your search.</p>
           </div>
-        )}
-      </For>
+        }
+      >
+        <For each={getFilteredCategories()}>
+          {({ name, slug, products }) => (
+            <div class="my-8">
+              <h1 class="text-2xl font-bold mb-4 scroll-mt-20" id={slug}>
+                {name}
+              </h1>
+              <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <For each={products}>
+                  {(product) => <Product product={product} />}
+                </For>
+              </div>
+            </div>
+          )}
+        </For>
+      </Show>
     </div>
   );
 };
