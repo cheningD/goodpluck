0ae337958350bc87639196d4e396eb5a4a33ede2
cheningd <cdduker@gmail.com>
2024-06-10 17:51:03 -0400

    allow using as many workers as cores on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -67,7 +67,7 @@ export default defineConfig({
   //   timeout: 10000,
   // },
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : 8,
+  workers: process.env.CI ? "100%" : "50%",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   webServer: process.env.CI
