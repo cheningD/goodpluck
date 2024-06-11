62fcd2e6260971fa3564ccb2561d142166ac2070
cheningd <cdduker@gmail.com>
2024-06-11 12:52:37 -0400

    Make sign in flow consistent, use TextInput

--- src/components/solid/TextInput.tsx
+++ src/components/solid/TextInput.tsx
@@ -20,21 +20,19 @@ export const TextInput: Component<TextInputProps> = (props) => {
   const [, inputProps] = splitProps(props, ["value", "label", "error"]);
 
   return (
-    <div class="mb-6">
+    <div class="mb-6 flex flex-col justify-stretch gap-2 w-full">
       {props.label && (
-        <label
-          for={props.name}
-          class="block text-base font-medium text-gray-700 mb-1"
-        >
+        <label for={props.name} class="text-gray-700">
           {props.label}
         </label>
       )}
       <input
         {...inputProps}
+        type={props.type}
         id={props.name}
         name={props.name}
         value={props.value ?? ""}
-        class={`mb-1 text-base py-3 px-4 block w-full border rounded shadow-md focus:ring-0 focus:outline-none ${
+        class={`border rounded-md p-2 w-full mb-1 focus:ring-0 focus:outline-none ${
           props.error
             ? "border-rose-500"
             : "border-zinc-400 focus:border-zinc-800"
